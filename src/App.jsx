4209c4f19a7e42ec5f427ae18197d57cbe3e450a
Moiseis Cruz <moizzito@gmail.com>
2025-01-18 18:00:54 -0300

    I created the route structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Form from "./pages/Form";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<Form />} />
+        <Route path="*" element={<h1>Error 404</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
