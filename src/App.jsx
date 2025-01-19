54857d422e77fde9c105f2712b0ab49feb1a90a0
Moiseis Cruz <moizzito@gmail.com>
2025-01-19 16:53:08 -0300

    I added the Header component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
 import GlobalStyles from "./components/GlobalStyles";
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
+
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<Form />} />
