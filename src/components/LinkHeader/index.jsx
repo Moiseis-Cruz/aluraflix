3e0aaf33cbd1df4c4cebe8ad522417103cc23d4d
Moiseis Cruz <moizzito@gmail.com>
2025-01-19 16:54:18 -0300

    I created the LinkHeader component function with all styling.

---
+++ src/components/LinkHeader/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { NavLink } from "react-router";
+import styled from "styled-components";
+
+const LinkButton = styled(NavLink)`
+    text-decoration: none;
+    width: 180px;
+    height: 54px;
+    border-radius: 15px;
+    display: inline-block;
+    font-size: 20px;
+    text-align: center;
+    line-height: 54px;
+    font-weight: 900;
+    color: #ffffff;
+    border: 2px solid #ffffff;
+    
+    &.active{
+        box-shadow: 0 0 5px 5px #2271D1 inset;
+        color: #2271D1;
+        border: 2px solid #2271D1;
+    }
+`
+
+const LinkHeader = ({ children, to }) => {
+
+    const [ acitve, setActive ] = useState("");
+
+    const handleClick = () => {
+        setActive(to);
+    }
+
+    return(
+        <LinkButton
+            to={to}
+            className={acitve === to ? "acitve" : ""}
+            onClick={handleClick}
+        >{children}</LinkButton>
+    )
+};
+
+export default LinkHeader;
