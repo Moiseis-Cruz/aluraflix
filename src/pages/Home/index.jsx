acaf31c37a7166a7204669e7c0c1d5268ac29250
Moiseis Cruz <moizzito@gmail.com>
2025-01-20 20:20:12 -0300

    I started the Home function and its styling.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,43 @@
+import styled from "styled-components";
+import Team from "../../components/Team";
+
+const ContainerHome = styled.section`
+    background-color: #000000E5;
+    padding: 93px 42px 159px;
+
+`
+
+const teams = [
+    {
+        nome: "FRONT END",
+        color: "#6BD1FF"
+    },
+    {
+        nome: "BACK END",
+        color: "#00C86F"
+    },
+    {
+        nome: "MOBILE",
+        color: "#FFBA05"
+    },
+];
+
 const Home = () => {
-    return "Home"
+    return (
+        <ContainerHome>
+            {
+                teams.map((team) => {
+                    return(
+                        <Team
+                            key={team.nome}
+                            nome={team.nome}
+                            color={team.color}
+                        />
+                    )
+                })
+            }
+        </ContainerHome>
+    )
 };
 
 export default Home;
