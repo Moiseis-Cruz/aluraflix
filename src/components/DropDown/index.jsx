dcdb5c7b01fb2b2bede007c04f14845ebd8fc895
Moiseis Cruz <moizzito@gmail.com>
2025-01-19 22:53:03 -0300

    I styled DropDown

--- src/components/DropDown/index.jsx
+++ src/components/DropDown/index.jsx
@@ -1,13 +1,32 @@
+import styled from "styled-components";
+
+const Select = styled.select`
+    width: 570px;
+    font-size: 20px;
+    font-weight: 600;
+    padding: 16px 12px;
+    border-radius: 10px;
+    border: 3px solid #262626;
+    background-color: transparent;
+    color: #A5A5A5;
+`
+
+const Label = styled.label`
+    font-size: 20px;
+    font-weight: 600;
+    display: block;
+`
+
 const DropDown = ({ label, teams }) => {
     return(
         <div>
-            <label>{label}</label>
-            <select>
-                <option value=""></option>
+            <Label>{label}</Label>
+            <Select>
+                <option value="" disabled selected >selecione uma categoría</option>
                 {
                     teams.map((team) => <option key={team}>{team}</option>)
                 }
-            </select>
+            </Select>
         </div>
     )
 };
