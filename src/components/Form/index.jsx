2e2f4f6615a0c9fadb02376571a57885c097b0cc
Moiseis Cruz <moizzito@gmail.com>
2025-01-19 22:53:42 -0300

    Minor changes to Form styling

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -6,12 +6,12 @@ const ContainerForm = styled.form`
     width: 1172px;
     max-width: 100%;
     margin-top: 47px;
+    font-family: "Source Sans 3", serif;
 `
 
 const TitleForm = styled.h3`
     font-size: 36px;
     font-weight: 600;
-    font-family: "Source Sans 3", serif;
     padding-block: 8px;
     border-block: 2px solid #262626;
 `
@@ -28,10 +28,12 @@ const Form = () => {
         <ContainerForm>
             <TitleForm>Criar Card</TitleForm>
 
-            <TextInput label="Título" placeholder="Título do vídeo" />
-            <DropDown label="Categoria" teams={team} />
-            <TextInput label="Imagem" placeholder="Link da imagem" />
-            <TextInput label="Vídeo" placeholder="Link do vídeo" />
+            <div style={{display: "flex", flexWrap: "wrap", gap: "60px 20px", marginTop: "60px"}}>
+                <TextInput label="Título" placeholder="Título do vídeo" />
+                <DropDown label="Categoria" teams={team} />
+                <TextInput label="Imagem" placeholder="Link da imagem" />
+                <TextInput label="Vídeo" placeholder="Link do vídeo" />
+            </div>
         </ContainerForm>
     )
 };
