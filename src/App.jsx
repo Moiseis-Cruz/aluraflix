3f2979eeedbb0a88164503e3b7a5739bd6bd50d0
Moiseis Cruz <moizzito@gmail.com>
2025-01-20 20:17:25 -0300

    I created and styled BackgroundContent.

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,28 @@ import FormSection from "./pages/FormSection";
 import GlobalStyles from "./components/GlobalStyles";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import styled from "styled-components";
 
+const BackgroundContent = styled.div`
+  width: 1440px;
+  max-width: 100%;
+  margin: 0 auto;
+`
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyles />
+      <BackgroundContent>
+        <GlobalStyles />
 
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/form" element={<FormSection />} />
-        <Route path="*" element={<h1>Error 404</h1>} />
-      </Routes>
-      <Footer />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<FormSection />} />
+          <Route path="*" element={<h1>Error 404</h1>} />
+        </Routes>
+        <Footer />
+      </BackgroundContent>
     </BrowserRouter>
   )
 }
