1fe3663f78c6f978cf30b6389d5f9e06a843a7c6
Moiseis Cruz <moizzito@gmail.com>
2025-01-19 22:55:36 -0300

    I styled TextInput.

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,9 +1,33 @@
+import styled from "styled-components";
+
+const FormControl = styled.div`
+    max-width: 573px;
+`
+
+const Input = styled.input`
+    width: 573px;
+    max-width: 100%;
+    font-size: 20px;
+    font-weight: 600;
+    padding: 16px 12px;
+    border-radius: 10px;
+    border: 3px solid #262626;
+    background-color: transparent;
+    color: #A5A5A5;
+`
+
+const Label = styled.label`
+    font-size: 20px;
+    font-weight: 600;
+    display: block;
+`
+
 const TextInput = ({ label, placeholder }) => {
     return(
-        <div>
-            <label>{label}</label>
-            <input type="text" placeholder={placeholder} />
-        </div>
+        <FormControl>
+            <Label>{label}</Label>
+            <Input type="text" placeholder={placeholder} />
+        </FormControl>
     )
 };
 
