f2a2ca6357733bfcfc9ff6c20ecdbb588f76a321
Moiseis Cruz <moizzito@gmail.com>
2025-01-21 18:57:37 -0300

    I styled the scroll bar.

--- src/components/GlobalStyles/index.jsx
+++ src/components/GlobalStyles/index.jsx
@@ -8,6 +8,23 @@ const GlobalStyles = createGlobalStyle`
         outline: none;
     }
 
+    *::-webkit-scrollbar {
+        /* width: 10px; */
+        height: 10px;
+    }
+    
+    *::-webkit-scrollbar-track {
+        background: #2271D12B;
+        border-radius: 20px;
+    }
+    
+    *::-webkit-scrollbar-thumb {
+        background-color: #2271D1;
+        border-radius: 20px;
+        border: 5px double #014291;
+        cursor: pointer;
+    }
+
     body {
         font-family: "Roboto", serif;
         background-color: #585858;
